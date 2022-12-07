4d39a63057e36f416a0a41bd7f8cd54b5ea404b9
meruem92 <me.achemlal.com>
2022-12-07 13:57:22 +0100

    init Next js

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Advice generator app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h1>
+          Lorem ipsum dolor sit amet.
+        </h1>
+      </div>
+
+    </div>
+  )
+}
